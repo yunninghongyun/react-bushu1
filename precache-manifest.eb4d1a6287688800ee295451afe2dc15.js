6253f901ab553964c2db6748e7e020a7ef8b9f86
yunninghongyun <786898343@qq.com>
2019-08-19 15:45:38 +0800

    bushu

---
+++ precache-manifest.eb4d1a6287688800ee295451afe2dc15.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49b56ff0fbaff22b815fdc230cce3b4c",
+    "url": "https//yunninghongyun.github.io/react-bushu1/index.html"
+  },
+  {
+    "revision": "9421dcc4158a4749d402",
+    "url": "https//yunninghongyun.github.io/react-bushu1/static/css/main.685dea8f.chunk.css"
+  },
+  {
+    "revision": "f927449ba678a204afe5",
+    "url": "https//yunninghongyun.github.io/react-bushu1/static/js/2.9b0ac4eb.chunk.js"
+  },
+  {
+    "revision": "9421dcc4158a4749d402",
+    "url": "https//yunninghongyun.github.io/react-bushu1/static/js/main.8236596c.chunk.js"
+  },
+  {
+    "revision": "dc2f9a954286cb44a268",
+    "url": "https//yunninghongyun.github.io/react-bushu1/static/js/runtime~main.675eaf85.js"
+  }
+]);
--- src/components/About.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-class About extends Component {
-  state = {}
-  render() {
-    return <div>About</div>
-  }
-}
-
-export default About
