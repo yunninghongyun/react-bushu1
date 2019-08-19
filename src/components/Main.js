bda7dd403ad445baf650bf51e517515f554f09a5
yunninghongyun <786898343@qq.com>
2019-08-19 15:16:48 +0800

    qqq

---
+++ src/components/Main.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
+import Home from './Home'
+import About from './About'
+class Main extends Component {
+  state = {}
+  render() {
+    return (
+      <div>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+      </div>
+    )
+  }
+}
+
+export default Main
